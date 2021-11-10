f6df238638ba9441cac90f5ae0c42b8a8f62c3a5
Christian <leech92@gmail.com>
2021-11-10 17:05:47 -0500

    Handling Submission Errors

--- first_r_c/app/controllers/users_controller.rb
+++ first_r_c/app/controllers/users_controller.rb
@@ -5,14 +5,27 @@ class UsersController < ApplicationController
         render json: @users
     end
 
-    def create
-        render json: params
-    end
+    # def create
+    #     render json: params
+    # end
 
     def show
-        render json: params
+        user = User.find(params[:id])
+        render json: user
     end
 
-    
+    def create
+        user = User.new(user_params)
+        # replace the `user_attributes_here` with the actual attribute keys
+        if user.save
+            redirect_to user_url(user)
+        else
+            render json: user.errors.full_messages, status:422
+        end
+    end
+
+    def user_params
+        params.require(:user).permit(:name, :email)
+    end
 
 end
