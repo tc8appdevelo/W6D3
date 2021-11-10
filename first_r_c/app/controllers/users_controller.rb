70414d2b17f5bb21a7a47e86634c571b6866db24
tc8appdevelo <tc8appdevelo@gmail.com>
2021-11-10 15:08:14 -0600

    created users to get

--- first_r_c/app/controllers/users_controller.rb
+++ first_r_c/app/controllers/users_controller.rb
@@ -1,5 +1,18 @@
 class UsersController < ApplicationController
     def index
-        render plain: "I'm in the index action!"
+        #render plain: "I'm in the index action!"
+        @users = User.all
+        render json: @users
     end
+
+    def create
+        render json: params
+    end
+
+    def show
+        render json: params
+    end
+
+    
+
 end
