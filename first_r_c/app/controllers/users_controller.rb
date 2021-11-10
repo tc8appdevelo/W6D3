e7c42bbd0b29070e07faaebd02ba5d15a218256a
tc8appdevelo <tc8appdevelo@gmail.com>
2021-11-10 16:39:30 -0600

    finished part 1

--- first_r_c/app/controllers/users_controller.rb
+++ first_r_c/app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
-    def index
-        #render plain: "I'm in the index action!"
-        @users = User.all
-        render json: @users
-    end
+    # def index
+    #     #render plain: "I'm in the index action!"
+    #     @users = User.all
+    #     render json: @users
+    # end
 
     # def create
     #     render json: params
@@ -26,6 +26,15 @@ class UsersController < ApplicationController
 
     def user_params
         params.require(:user).permit(:name, :email)
+    end
+
+    def update
+        user = User.find(params[:id])
+        if user.update(user_params)
+            redirect_to user_url(user)
+        else
+            render json: user.errors.full_messages, status:422
+        end
     end
 
 end
